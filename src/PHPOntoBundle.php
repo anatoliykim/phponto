9c9637c3bb00cc1e2dce2610930ae00d813f3128
Anatoliy Kim <anatoliykim@users.noreply.github.com>
2019-09-27 00:15:47 +0300

    bundle

--- src/PHPOntoBundle.php
+++ src/PHPOntoBundle.php
@@ -1,1 +1,15 @@
 <?php
+
+namespace AnatoliyKim\PHPOnto;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Class PHPOntoBundle
+ *
+ * @package AnatoliyKim\PHPOnto
+ */
+class PHPOntoBundle extends Bundle
+{
+
+}
